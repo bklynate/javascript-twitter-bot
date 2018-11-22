a83b51c014526267f45f2ade7d86ca5aebe18fd9
bklynate <nathanielcfa@gmail.com>
2018-11-22 11:45:16 -0500

    add babel-plugin-transform-runtime to project

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,6 @@
-/*
-  Goals:
-
-  1. Connect to Twitter - FINISHED
-  2. Run a search for a topic - FINISHED
-  3. choose a random tweet from search results - FINISHED
-  4. confirm the tweet is a new tweet - IN PROGRESS
-  5. post tweet on a twitter accout - FINISHED
-    - need to grab more than 15 tweets to choose from - FINISHED
-  6. run the code every x amount of mins - FINISHED
-  7. figure out how to create a worker with nodejs on heroku - FINISHED
-  8. FUTURE WILD GOAL: add in sentiment analysis in some manner
-*/
-
 import Twitter from 'twitter';
-import {
-  consumer_key,
-  consumer_secret,
-  access_token_key,
-  access_token_secret,
-} from './config/index';
+import { consumer_key, consumer_secret, access_token_key, access_token_secret } from './config/index';
+import topicalTwitterSearchPhrases from './config/topicalTwitterSearchPhrases';
 
 const client = new Twitter({
   consumer_key,
@@ -39,31 +21,6 @@ function getRandomElementIndex(arr) {
 }
 
 function getSearchPhrase() {
-  const topicalTwitterSearchPhrases = [
-    'free coding resources',
-    'free javascript resources',
-    'free coding tutorials',
-    'tutorial javascript es6',
-    'react tutorials',
-    'full stack react tutorials',
-    'learn react.js',
-    'node js tutorials',
-    'you dont know js',
-    'learn redux',
-    'learn GraphQL',
-    'learn coding for free',
-    'learn node js',
-    'express js node resources',
-    'javascript game development',
-    'NYC Tech Pipline',
-    'Free Code Camp',
-    'NYC javascript meetups',
-    'react native tutorial',
-    'brooklyn.js',
-    'nyc hackathon',
-    '30 days of javascript',
-    '100 days of code',
-  ];
   // this is the random element used to select a possible search query
   const randomTopicalTwitterSearchIndex = getRandomElementIndex(topicalTwitterSearchPhrases);
   const topicalTweetPhrase = topicalTwitterSearchPhrases[randomTopicalTwitterSearchIndex];
@@ -101,4 +58,4 @@ const twitterBotEngine = async function() {
 };
 
 twitterBotEngine();
-setInterval(twitterBotEngine, 30000);
+setInterval(twitterBotEngine, 16200000);
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,4 @@
-export default {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-}
+export const consumer_key = process.env.CONSUMER_KEY;
+export const consumer_secret = process.env.CONSUMER_SECRET;
+export const access_token_key = process.env.ACCESS_TOKEN_KEY;
+export const access_token_secret = process.env.ACCESS_TOKEN_SECRET;

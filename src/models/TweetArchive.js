7740f8541fa1e2361a211b0f2fc1045cadd4aa95
bklynate <nathanielcfa@gmail.com>
2018-12-27 21:20:30 -0500

    add "created_at" to tweetArchive schema

--- src/models/TweetArchive.js
+++ src/models/TweetArchive.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const TweetArchiveSchema = new Schema({
-  id: String,    
+  id: String,
   text: String,
   name: String,
   screen_name: String,
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
 });
 
 mongoose.model('tweetArchive', TweetArchiveSchema);
